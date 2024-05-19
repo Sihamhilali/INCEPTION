0cd42135ceb8aa0bd17a23509c73314fa1b0f869
Siham Elhilali <selhilal@e1r4p12.1337.ma>
2024-05-19 16:07:31 +0100

    still not working

--- srcs/requirements/wordpress/tools/Script.sh
+++ srcs/requirements/wordpress/tools/Script.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 cd /var/www/html
-curl -O https://wordpress.org/latest.zip
-unzip latest.zip
+wp core download --allow-root
+wp config create --allow-root --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=mariadb
+wp core install --url=$DOMAIN_NAME --title=Inception --admin_user=supervisor --admin_password=strongpassword --admin_email=info@example.com --allow-root
+service php7.4-fpm start
+sed -i "s/^listen = .*/listen = 0.0.0.0:9000/" /etc/php/7.4/fpm/pool.d/www.conf
+service php7.4-fpm stop
 
-exec "$@"
+php-fpm7.4 -F
