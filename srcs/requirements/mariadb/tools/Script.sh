a934f1e8329be1fecc23f8eb0d9d80e339c3c60b
Siham Elhilali <selhilal@e1r4p12.1337.ma>
2024-05-20 22:28:53 +0100

    push

--- srcs/requirements/mariadb/tools/Script.sh
+++ srcs/requirements/mariadb/tools/Script.sh
@@ -1,12 +1,26 @@
+
 #!/bin/bash
 
-service mysql start
-sed -i 's/bind-address            = 127.0.0.1/bind-address = 0.0.0.0/g' /etc/mysql/mariadb.conf.d/50-server.cnf
-mysql -u root -p$MYSQL_ROOTPASSWORD -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
-mysql -u root -p$MYSQL_ROOTPASSWORD -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
-mysql -u root -p$MYSQL_ROOTPASSWORD -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';"
-mysql -u root -p$MYSQL_ROOTPASSWORD -e "FLUSH PRIVILEGES;"
-mysql -u root -p$MYSQL_ROOTPASSWORD -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOTPASSWORD';"
-kill `cat /var/run/mysqld/mysqld.pid`
+service mariadb start
+sleep 5
+sed -i 's/bind-address\s*=\s*127.0.0.1/bind-address = 0.0.0.0/g' /etc/mysql/mariadb.conf.d/50-server.cnf
 
-exec "$@"
+# cat << EOF > /usr/share/init.sql
+# ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+# CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
+# CREATE USER IF NOT EXISTS ${MYSQL_USER}@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+# GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* to ${MYSQL_USER}@'%' WITH GRANT OPTION;
+# FLUSH PRIVILEGES;
+# EOF
+
+# mysql -u root < /usr/share/init.sql
+
+mariadb -uroot -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};"
+mariadb -uroot -e "CREATE USER IF NOT EXISTS ${MYSQL_USER}@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mariadb -uroot -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* to ${MYSQL_USER}@'%'
+WITH GRANT OPTION;"
+mariadb -uroot -e "FLUSH PRIVILEGES;"
+#mariadb -uroot -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+
+service mariadb stop
+mariadbd
--- srcs/requirements/wordpress/tools/Script.sh
+++ srcs/requirements/wordpress/tools/Script.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
+sleep 15
+
+if [ -f /var/www/html/wp-config.php ]; then
+    echo "wordpress already installed"
+else
+mkdir -p /var/www/html
+
 cd /var/www/html
+
+
+rm -rf *
+
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar 
+
+chmod +x wp-cli.phar 
+
+mv wp-cli.phar /usr/local/bin/wp
+
 wp core download --allow-root
 wp config create --allow-root --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=mariadb
 wp core install --url=$DOMAIN_NAME --title=Inception --admin_user=supervisor --admin_password=strongpassword --admin_email=info@example.com --allow-root
-service php7.4-fpm start
 sed -i "s/^listen = .*/listen = 0.0.0.0:9000/" /etc/php/7.4/fpm/pool.d/www.conf
 service php7.4-fpm stop
 
+# wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PWD --allow-root
+fi
 php-fpm7.4 -F
