bca0cae9379257653acd66846093dad1162a7a42
Siham Elhilali <selhilal@e1r4p12.1337.ma>
2024-05-23 21:53:24 +0100

    push

--- srcs/requirements/mariadb/tools/Script.sh
+++ srcs/requirements/mariadb/tools/Script.sh
@@ -24,12 +24,12 @@ fi
 service mariadb start
 sleep 5
 sed -i 's/bind-address\s*=\s*127.0.0.1/bind-address = 0.0.0.0/g' /etc/mysql/mariadb.conf.d/50-server.cnf
+mysqladmin -uroot password "${MYSQL_ROOT_PASSWORD}"
 mariadb -uroot -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};"
 mariadb -uroot -e "CREATE USER IF NOT EXISTS ${MYSQL_USER}@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
 mariadb -uroot -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* to ${MYSQL_USER}@'%'
 WITH GRANT OPTION;"
 mariadb -uroot -e "FLUSH PRIVILEGES;"
-#mariadb -uroot -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
 
 service mariadb stop
 mariadbd
--- srcs/requirements/wordpress/tools/Script.sh
+++ srcs/requirements/wordpress/tools/Script.sh
@@ -2,27 +2,26 @@
 sleep 15
 
 if [ -f /var/www/html/wp-config.php ]; then
-    echo "wordpress already installed"
+    echo "WordPress already installed"
 else
-mkdir -p /var/www/html
+    mkdir -p /var/www/html
+    cd /var/www/html
 
-cd /var/www/html
+    rm -rf *
 
+    curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 
-rm -rf *
+    chmod +x wp-cli.phar
 
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar 
+    mv wp-cli.phar /usr/local/bin/wp
 
-chmod +x wp-cli.phar 
-
-mv wp-cli.phar /usr/local/bin/wp
-
-wp core download --allow-root
-wp config create --allow-root --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=mariadb
-wp core install --url=$DOMAIN_NAME --title=Inception --admin_user=supervisor --admin_password=strongpassword --admin_email=info@example.com --allow-root
-sed -i "s/^listen = .*/listen = 0.0.0.0:9000/" /etc/php/7.4/fpm/pool.d/www.conf
-service php7.4-fpm stop
-
-# wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PWD --allow-root
+    wp core download --allow-root
+    wp config create --allow-root --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=mariadb
+    wp core install --url=$DOMAIN_NAME --title=Inception --admin_user=$ADMINE_USER --admin_password=$USER_PASS --admin_email=$EMAIL_PRS --allow-root
+    
+    sed -i "s/^listen = .*/listen = 0.0.0.0:9000/" /etc/php/7.4/fpm/pool.d/www.conf
+    service php7.4-fpm stop
+    wp user create newuser $EMAIL_PRS--role=editor --user_pass=$USER_PASS --allow-root
 fi
+
 php-fpm7.4 -F
