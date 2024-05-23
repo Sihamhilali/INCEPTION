fe222afa4e894822977b8ff201a043816eda2f6b
Siham Elhilali <selhilal@e1r4p12.1337.ma>
2024-05-23 21:19:34 +0100

    push

--- srcs/requirements/mariadb/tools/Script.sh
+++ srcs/requirements/mariadb/tools/Script.sh
@@ -1,20 +1,29 @@
 
 #!/bin/bash
 
+if [ -z "$MYSQL_DATABASE" ]; then
+  echo "Error: MYSQL_DATABASE is not set."
+  exit 1
+fi
+
+if [ -z "$MYSQL_ROOT_PASSWORD" ]; then
+  echo "Error: MYSQL_ROOT_PASSWORD is not set."
+  exit 1
+fi
+
+if [ -z "$MYSQL_USER" ]; then
+  echo "Error: MYSQL_USER is not set."
+  exit 1
+fi
+
+if [ -z "$MYSQL_PASSWORD" ]; then
+  echo "Error: MYSQL_PASSWORD is not set."
+  exit 1
+fi
+
 service mariadb start
 sleep 5
 sed -i 's/bind-address\s*=\s*127.0.0.1/bind-address = 0.0.0.0/g' /etc/mysql/mariadb.conf.d/50-server.cnf
-
-# cat << EOF > /usr/share/init.sql
-# ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
-# CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
-# CREATE USER IF NOT EXISTS ${MYSQL_USER}@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
-# GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* to ${MYSQL_USER}@'%' WITH GRANT OPTION;
-# FLUSH PRIVILEGES;
-# EOF
-
-# mysql -u root < /usr/share/init.sql
-
 mariadb -uroot -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};"
 mariadb -uroot -e "CREATE USER IF NOT EXISTS ${MYSQL_USER}@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
 mariadb -uroot -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* to ${MYSQL_USER}@'%'
