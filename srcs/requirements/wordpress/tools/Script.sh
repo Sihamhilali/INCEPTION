070bb534a678d415154eb76a4b5e0fe28d5c49a6
Siham Elhilali <selhilal@e1r4p12.1337.ma>
2024-05-30 23:43:01 +0100

    <3

--- srcs/requirements/wordpress/tools/Script.sh
+++ srcs/requirements/wordpress/tools/Script.sh
@@ -17,11 +17,11 @@ else
 
     wp core download --allow-root
     wp config create --allow-root --dbname=$MYSQL_DATABASE --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=mariadb
-    wp core install --url=$DOMAIN_NAME --title=Inception --admin_user=$ADMINE_USER --admin_password=$USER_PASS --admin_email=$EMAIL_PRS --allow-root
+    wp core install --url=$DOMAIN_NAME --title=Inception --admin_user=$ADMIN_USER --admin_password=$USER_PASS --admin_email=$ADMIN_EMAIL --allow-root
     
     sed -i "s/^listen = .*/listen = 0.0.0.0:9000/" /etc/php/7.4/fpm/pool.d/www.conf
     service php7.4-fpm stop
-    wp user create $ADMINE_USER $EMAIL_PRS --role=subscriber --user_pass=$USER_PASS --allow-rootfi
+    wp user create $USER_NAME $EMAIL_PRS --role=subscriber --user_pass=$USER_PASS --allow-root
 fi
 
 php-fpm7.4 -F
